582f4e5947bfd97429eda28226b35fdde14b1347
markokroselj <kroselj.marko123@gmail.com>
2024-05-26 18:31:28 +0200

    fixed links

--- view/components/header.php
+++ view/components/header.php
@@ -1,15 +1,17 @@
-<?php 
-$link =  $authenticated  ?  '<a href="logout">Logout</a>' : '<a href="login">Login</a>'; 
+<?php
+$link =  $authenticated  ?  '<a href="' . BASE_URL . 'logout">Logout</a>' : '<a href="' . BASE_URL . 'login">Login</a>';
 ?>
 <header class="flex justify-between p-4">
     <div>
-        <a href="<?php BASE_URL ?>home"><h1 class="text-3xl font-bold">Launch tracker</h1></a>
+        <a href="<?= BASE_URL ?>home">
+            <h1 class="text-3xl font-bold">Launch tracker</h1>
+        </a>
     </div>
     <nav>
         <ul class="flex gap-3">
-            <li><a href="about">About</a></li>
+            <li><a href="<?= BASE_URL ?>about">About</a></li>
             <li><?= $link ?></li>
-            <li><a href="dashboard">Dashboard</a></li>
+            <li><a href="<?= BASE_URL ?>dashboard">Dashboard</a></li>
         </ul>
     </nav>
-  </header>
+</header>
